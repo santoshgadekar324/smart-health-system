0694732ff5c5d21f1085c6cdafab41267158dd44
santoshgadekar324 <139868038+santoshgadekar324@users.noreply.github.com>
2026-03-17 12:26:42 +0530

    Update and rename SecurityConfig.java to AppSecurityConfig.java

--- smart-health-backend/src/main/java/com/smarthealth/config/AppSecurityConfig.java
+++ smart-health-backend/src/main/java/com/smarthealth/config/AppSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class AppSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
