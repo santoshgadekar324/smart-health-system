f4a58ac7def99a3132f11f000f607df959f007a0
santoshgadekar324 <139868038+santoshgadekar324@users.noreply.github.com>
2026-03-18 19:53:56 +0530

    Update JwtAuthenticationFilter.java

--- smart-health-backend/src/main/java/com/smarthealth/security/JwtAuthenticationFilter.java
+++ smart-health-backend/src/main/java/com/smarthealth/security/JwtAuthenticationFilter.java
@@ -26,6 +26,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getServletPath();
+
+// ✅ Skip login & register endpoints
+if (path.startsWith("/api/auth")) {
+    filterChain.doFilter(request, response);
+    return;
+}
         try {
             String jwt = getJwtFromRequest(request);
 
