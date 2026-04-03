c7b62bd797d92a8410697b9a396104e6f4b81024
santoshgadekar324 <139868038+santoshgadekar324@users.noreply.github.com>
2026-04-03 20:11:18 +0530

    Update patient.js

--- frontend/patient.js
+++ frontend/patient.js
@@ -285,24 +285,6 @@ document.querySelectorAll(".sidebar-link").forEach(link => {
   });
 
 });
-function loadSymptoms() {
-  const symptoms = ["fever", "cough", "headache"];
-
-  const container = document.getElementById("symptomGrid");
-
-  container.innerHTML = "";
-
-  symptoms.forEach(s => {
-    container.innerHTML += `
-      <label>
-        <input type="checkbox" class="symptom" value="${s}"> ${s}
-      </label><br>
-    `;
-  });
-}
-
-loadSymptoms();
-
 // =====================
 // LOAD ON START
 // =====================
